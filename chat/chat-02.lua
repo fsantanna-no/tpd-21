4b8becfd0cca93a3167a6f8975cb561eee05e37f
Francisco Sant'Anna <francisco.santanna@gmail.com>
2022-04-26 09:18:00 -0300

    - chat

--- chat/chat-02.lua
+++ chat/chat-02.lua
@@ -23,16 +23,16 @@ end
 for l in io.lines('wikimedia.chat') do
     l = string.gsub(l, "'", " ")
     assert(not string.find(l, "'"))
-    local y,m,d,hh,mm,ss,user,msg = string.match(l, "(%d%d%d%d)(%d%d)(%d%d) %[(%d%d):(%d%d):(%d%d)%] %<(%a+)%>\t(.*)")
+    local y,m,d,hh,mm,ss,user,msg = string.match(l, "(%d%d%d%d)(%d%d)(%d%d) %[(%d%d):(%d%d):(%d%d)%] %<([%a%d-_]+)%>\t(.*)")
     RETS[y~=nil] = RETS[y~=nil] + 1
     if y then
         assert(y and m and d and hh and mm and ss and user and msg)
         --print(y , m , d , hh , mm , ss , user , msg)
-        local t = USERS[user] or { n=0, user=user, key=nil, likes=0, xxx={}, yyy={} }
+        local t = USERS[user] or { n=0, user=user, keys=nil, likes=0, xxx={}, yyy={} }
         USERS[user] = t
         t.n = t.n + 1
-        if not t.key then
-            local cmd = "freechains --port=8401 key pubpvt " .. t.user
+        if not t.keys then
+            local cmd = "freechains --port=8401 keys pubpvt " .. t.user
             --print(cmd)
             local v = exec(cmd)
             local pub,pvt = string.match(v, "([^ ]*) ([^ ]*)")
@@ -72,8 +72,8 @@ for l in io.lines('wikimedia.chat') do
             end
         end
     end
-    if N == 2000 then
-        break
+    if N == 5000 then
+        --break
     end
     N = N + 1
 end
@@ -153,5 +153,109 @@ TrevorParscal	4	6   *
       Ashlee	0	33
 
 15	users
+
+# 5000 messages
+
+4153	847
+ RoanKattouw	62	1060
+        jorm	42	738
+       Reedy	38	345
+     kaldari	31	77
+      werdna	24	127
+       robla	23	49
+        aude	18	18
+  Nikerabbit	17	21
+ flipzagging	16	384
+   Jamesofur	13	13
+     guillom	12	175
+     tomaszf	11	21
+       NickK	9	9
+TrevorParscal	8	305
+ AryehGregor	8	11
+       mdale	7	40
+  Platonides	6	15
+       thedj	6	38
+       brion	5	23
+      Tanvir	4	4
+     alolita	4	12
+        RobH	4	11
+     pdhanda	4	17
+    roberthl	3	5
+       reedy	2	2
+      basile	2	7
+     EcceNux	2	2
+    bastique	2	2
+     bawolff	2	3
+     Otourly	2	2
+   chilliboy	2	2
+ killiondude	2	2
+     emrovic	1	1
+      danese	1	7
+   Dispenser	1	1
+     Romaine	1	1
+         vvv	1	2
+    parutron	1	1
+      Ashlee	0	600
+39	users	4153	397
+
+# 10000
+
+8625	1375
+ flipzagging	106	1161
+ RoanKattouw	86	2067
+     Shirley	46	91
+        jorm	42	1024
+TrevorParscal	39	970
+       Reedy	38	525
+     kaldari	31	77
+      werdna	24	211
+     jeremyb	23	25
+       robla	23	156
+     atglenn	20	20
+    Dodoiste	19	43
+        aude	18	50
+    Diederik	17	23
+  Nikerabbit	17	58
+        Skud	15	15
+   Dispenser	14	54
+     apergos	13	13
+   Jamesofur	13	13
+     guillom	12	452
+ aokomoriuta	11	11
+     tomaszf	11	21
+       Bryan	10	49
+       NickK	9	9
+     EcceNux	9	12
+ killiondude	8	13
+    YairRand	8	8
+ AryehGregor	8	13
+       mdale	7	81
+  Platonides	6	37
+       thedj	6	65
+        RobH	6	19
+       brion	5	76
+      Tanvir	4	4
+     pdhanda	4	33
+     alolita	4	172
+      howief	4	4
+        zakg	4	7
+    roberthl	3	14
+      asalga	3	3
+      basile	2	12
+   chilliboy	2	2
+     bawolff	2	6
+     Otourly	2	2
+     molliug	2	2
+    bastique	2	2
+       reedy	2	2
+     emrovic	1	1
+     Romaine	1	1
+       drdee	1	1
+    parutron	1	1
+     Krinkle	1	11
+         vvv	1	5
+      danese	1	7
+      Ashlee	0	871
+55	users	8625	767
 
 ]]
