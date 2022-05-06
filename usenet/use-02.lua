deb82ec85077a96906451d60deeb28f1ac6a69f7
Francisco Sant'Anna <francisco.santanna@gmail.com>
2022-05-06 11:06:04 -0300

    ...

--- usenet/use-02.lua
+++ usenet/use-02.lua
@@ -1,17 +1,97 @@
 #!/usr/bin/env lua5.3
 
 --[[
-15235
-TIMES=1584 | t2=98 | t3=142 | t5=176 | t7=1167 (1027,140) | t8=1 | tnegs=1023=862+160
-SIZES | fz=15236/3007/200/0 | negs=636x1036=658896 | xpnds=1/0
 
-19907
-TIMES=3336 | t2=128 | t3=219 | t5=289 | t7=2700 (2439,261) | t8=0 | tnegs=2436=2076+358
-SIZES | fz=19908/3740/1208/0 | negs=881x1465=1290665 | xpnds=1/0
+===============================================================================
 
-23860
-TIMES=3851 | t2=160 | t3=254 | t5=440 | t7=2996 (2616,379) | t8=1 | tnegs=2609=2277+331
-SIZES | fz=23862/4686/432/0 | negs=748x1215=908820 | xpnds=1/0
+3040
+TIMES=10 | t2=1 | t3=0 | t4=5 | t5=1 | t6=0 | t7=3 | t8=0 | tnegs=3
+SIZES | fz=3041/31 | negs=51x85=4335 | xpnds=1/0
+
+8452
+TIMES=26 | t2=2 | t3=0 | t4=14 | t5=3 | t6=1 | t7=6 | t8=0 | tnegs=6
+SIZES | fz=8452/29 | negs=51x75=3825 | xpnds=1/0
+
+9535
+TIMES=37 | t2=3 | t3=0 | t4=16 | t5=3 | t6=1 | t7=14 | t8=0 | tnegs=12
+SIZES | fz=9536/101 | negs=51x82=4182 | xpnds=1/0
+
+10802
+TIMES=33 | t2=3 | t3=0 | t4=19 | t5=4 | t6=1 | t7=6 | t8=0 | tnegs=6
+SIZES | fz=10802/33 | negs=51x81=4131 | xpnds=1/0
+
+11608
+TIMES=41 | t2=3 | t3=1 | t4=19 | t5=4 | t6=1 | t7=12 | t8=1 | tnegs=9
+SIZES | fz=11608/101 | negs=78x120=9360 | xpnds=1/0
+
+13063
+TIMES=65 | t2=5 | t3=0 | t4=24 | t5=4 | t6=2 | t7=29 | t8=1 | tnegs=25
+SIZES | fz=13064/101 | negs=259x407=105413 | xpnds=1/0
+
+13712
+TIMES=257 | t2=4 | t3=1 | t4=23 | t5=4 | t6=2 | t7=223 | t8=0 | tnegs=214
+SIZES | fz=13713/101 | negs=636x1036=658896 | xpnds=1/0
+
+===============================================================================
+
+386
+TIMES=4 | t2=0 | t3=0 | t4=0 | t5=1 | t6=0 | t7=3 (0,3,0) | t8=0 | tnegs=2
+SIZES | fz=387/70 | negs=51x94=4794 | xpnds=71/0
+
+2945
+TIMES=11 | t2=1 | t3=0 | t4=5 | t5=1 | t6=0 | t7=4 (0,4,0) | t8=0 | tnegs=4
+SIZES | fz=2945/21 | negs=101x173=17473 | xpnds=20/0
+
+4098
+TIMES=13 | t2=0 | t3=1 | t4=7 | t5=1 | t6=1 | t7=3 (0,3,0) | t8=0 | tnegs=3
+SIZES | fz=4099/34 | negs=51x89=4539 | xpnds=34/0
+
+6058
+TIMES=22 | t2=1 | t3=1 | t4=10 | t5=2 | t6=1 | t7=7 (0,7,0) | t8=0 | tnegs=7
+SIZES | fz=6059/101 | negs=51x84=4284 | xpnds=101/0
+
+6871
+TIMES=24 | t2=2 | t3=0 | t4=12 | t5=2 | t6=1 | t7=7 (0,7,0) | t8=0 | tnegs=7
+SIZES | fz=6870/76 | negs=51x75=3825 | xpnds=75/0
+
+7637
+TIMES=26 | t2=2 | t3=0 | t4=13 | t5=3 | t6=1 | t7=7 (0,7,0) | t8=0 | tnegs=7
+SIZES | fz=7638/64 | negs=51x79=4029 | xpnds=64/0
+
+10843
+TIMES=37 | t2=2 | t3=1 | t4=19 | t5=4 | t6=1 | t7=10 (0,10,0) | t8=0 | tnegs=10
+SIZES | fz=10843/74 | negs=51x81=4131 | xpnds=73/0
+
+12081
+TIMES=42 | t2=3 | t3=0 | t4=20 | t5=4 | t6=1 | t7=13 (0,13,0) | t8=1 | tnegs=13
+SIZES | fz=12082/51 | negs=101x164=16564 | xpnds=52/0
+
+12699
+TIMES=41 | t2=3 | t3=0 | t4=22 | t5=5 | t6=1 | t7=10 (0,10,0) | t8=0 | tnegs=10
+SIZES | fz=12700/50 | negs=51x76=3876 | xpnds=51/0
+
+13108
+TIMES=44 | t2=3 | t3=0 | t4=23 | t5=5 | t6=1 | t7=12 (0,12,0) | t8=0 | tnegs=12
+SIZES | fz=13108/77 | negs=51x85=4335 | xpnds=76/0
+
+18697
+TIMES=72 | t2=4 | t3=0 | t4=34 | t5=6 | t6=2 | t7=25 (0,25,0) | t8=1 | tnegs=25
+SIZES | fz=18698/85 | negs=51x72=3672 | xpnds=86/0
+
+24883
+TIMES=89 | t2=5 | t3=1 | t4=51 | t5=7 | t6=2 | t7=23 (0,21,1) | t8=0 | tnegs=21
+SIZES | fz=24884/74 | negs=51x79=4029 | xpnds=74/0
+
+25725
+TIMES=87 | t2=5 | t3=1 | t4=47 | t5=8 | t6=3 | t7=23 (0,23,0) | t8=0 | tnegs=23
+SIZES | fz=25726/73 | negs=51x79=4029 | xpnds=74/0
+
+26382
+TIMES=92 | t2=5 | t3=0 | t4=51 | t5=8 | t6=3 | t7=24 (0,24,0) | t8=1 | tnegs=24
+SIZES | fz=26383/101 | negs=91x147=13377 | xpnds=102/0
+
+
+===============================================================================
 
 ]]
 
@@ -39,6 +119,7 @@ function read_until (patt)
 end
 
 local TS = 0
+local reps = 0
 
 while true do
     local from = read_until("^From: (.*)")
@@ -68,7 +149,7 @@ while true do
             M = MON[m]
         else
             local p = "(%d+)/(%d+)/(%d+)"
-            y,d,M = string.match(date, p)
+            y,M,d = string.match(date, p)
             hh,mm = 0,0
         end
         assert(d)
@@ -79,8 +160,11 @@ while true do
         local ts = os.time({year=y, month=M, day=d, hour=hh, min=mm, sec=0})
         if ts > TS then
             TS = ts
+--print(date)
+--reps = reps + 1
         else
-            TS = TS + 1
+--print('',date)
+            TS = TS + 60
         end
     end
 
@@ -106,4 +190,5 @@ while true do
 ]]
 end
 
+print(reps)
 epilogue()
